3300beb31850b07046cad1e72cb2dd6fe760a9e2
root <youssef.mecky96@gmail.com>
2022-08-17 12:31:24 +0000

    added test dataloader and load data method for it

--- data_loading.py
+++ data_loading.py
@@ -48,13 +48,31 @@ class HistopathologicCancerDetectionDataset(Dataset):
             self.KAGGLE_DATASET, path=self.data_path, quiet=False
         )
 
+class HistopathologicCancerDetectionDataset_test(Dataset):
+    KAGGLE_DATASET = "histopathologic-cancer-detection"
 
+    def __init__(self, data_path,figsize=224):
+        self.data_path = data_path
+        self.test_id = pd.read_csv(self.zip_file.open("sample_submission.csv")) 
+        
+        self.transforms = tv_transforms.Compose([tv_transforms.Resize(figsize),
+                                              tv_transforms.ToTensor()],
+                                             )
+
+    def __len__(self):
+        return len(self.test_id)
+
+    def __getitem__(self, index):
+        image_id = self.test_id["id"].iloc[index]
+        image_file=self.zip_file.open(os.path.join("test", image_id + ".tif"))
+        img = Image.open(image_file)
+        return self.transforms(img), image_id
 def load_data(
     data_path=None,
     download=False,
     transforms=[],
     test_split=0.33,
-    batch_size=32,
+    batch_size=64,
     first_n_rows=0,
 ):
     """
@@ -76,7 +94,27 @@ def load_data(
     train_size = len(dataset) - test_size
     train_set, test_set = random_split(dataset, [train_size, test_size])
     train_loader = DataLoader(
-        train_set, shuffle=True, batch_size=batch_size, num_workers=1
+        train_set, shuffle=True, batch_size=batch_size, num_workers=2
     )
-    test_loader = DataLoader(test_set, batch_size=batch_size, num_workers=4)
+    test_loader = DataLoader(test_set, batch_size=batch_size, num_workers=2)
     return train_loader, test_loader
+
+def load_data_test(
+    data_path=None,
+    download=False,
+    transforms=[],
+    batch_size=64,
+    first_n_rows=0,
+):
+    """
+    Creates data_loader for test dataset
+    """
+    if not data_path:
+        data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test")
+    dataset = HistopathologicCancerDetectionDataset_test(
+        data_path,transforms=transforms
+    )
+    
+    test_loader = DataLoader(dataset, batch_size=batch_size, num_workers=2)
+    return test_loader
+
