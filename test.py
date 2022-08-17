e4ed38a3664ed00c44acba78072c154a239c4042
root <youssef.mecky96@gmail.com>
2022-08-17 12:50:30 +0000

    fixed bug with models

--- test.py
+++ test.py
@@ -12,6 +12,7 @@ from utils import WANDB_PROJECT_NAME, get_device, DEFAULT_DATA_PATH
 import sklearn.metrics
 import numpy as np
 import csv
+import os
 # TODO: divide by total
 # TODO: Use actual validation
 # TODO: Submit to kaggle
@@ -20,53 +21,53 @@ import csv
 
 
 def test(model, test_loader, device,mode='val'):
-  if mode =='val':
+    if mode =='val':
+        wandb.watch(model, log_freq=100)
+
+        test_size = len(test_loader.dataset)
+        batch_size = test_loader.batch_size
+        running_accuracy = 0
+
+        y_true = np.empty(test_size)
+        y_predicated = np.empty(test_size)
+        with torch.no_grad():
+            for i, (input_images, output_labels) in enumerate(test_loader):
+                input_images.to(device)
+                output_labels.type(torch.LongTensor).to(device)
+
+                predicted_outputs = model(input_images)
+                _, predicted = torch.max(predicted_outputs, 1)
+                y_true[i * batch_size : (i + 1) * batch_size] = output_labels
+                y_predicated[i * batch_size : (i + 1) * batch_size] = predicted
+
+                # there really isn't a point for this but let's try it anyway
+                running_accuracy += (predicted == output_labels).sum().item()
+                wandb.log({"accuracy": running_accuracy})
+
+        f1 = sklearn.metrics.f1_score(y_true, y_predicated)
+        recall = sklearn.metrics.recall_score(y_true, y_predicated)
+        precision = sklearn.metrics.precision_score(y_true, y_predicated)
+        auc = sklearn.metrics.roc_auc_score(y_true, y_predicated)
+        accuracy = sklearn.metrics.accuracy_score(y_true, y_predicated)
+
+        # OR to log a final metric at the end of training you can also use wandb.summary
+
+        # https://docs.wandb.ai/guides/integrations/scikit
+        wandb.run.summary["accuracy"] = accuracy
+        wandb.run.summary["f1"] = f1
+        wandb.run.summary["recall"] = recall
+        wandb.run.summary["precision"] = precision
+        wandb.run.summary["auc"] = auc
+        wandb.sklearn.plot_roc(y_true, y_predicated, [0, 1])
+        wandb.log(
+            {
+                "conf_matrix": wandb.plot.confusion_matrix(
+                    probs=None, y_true=y_true, preds=y_predicated, class_names=[0, 1],
+                )
+            }
+        )
+    elif mode=='test':
       wandb.watch(model, log_freq=100)
-
-      test_size = len(test_loader.dataset)
-      batch_size = test_loader.batch_size
-      running_accuracy = 0
-
-      y_true = np.empty(test_size)
-      y_predicated = np.empty(test_size)
-      with torch.no_grad():
-          for i, (input_images, output_labels) in enumerate(test_loader):
-              input_images.to(device)
-              output_labels.type(torch.LongTensor).to(device)
-
-              predicted_outputs = model(input_images)
-              _, predicted = torch.max(predicted_outputs, 1)
-              y_true[i * batch_size : (i + 1) * batch_size] = output_labels
-              y_predicated[i * batch_size : (i + 1) * batch_size] = predicted
-
-              # there really isn't a point for this but let's try it anyway
-              running_accuracy += (predicted == output_labels).sum().item()
-              wandb.log({"accuracy": running_accuracy})
-
-      f1 = sklearn.metrics.f1_score(y_true, y_predicated)
-      recall = sklearn.metrics.recall_score(y_true, y_predicated)
-      precision = sklearn.metrics.precision_score(y_true, y_predicated)
-      auc = sklearn.metrics.roc_auc_score(y_true, y_predicated)
-      accuracy = sklearn.metrics.accuracy_score(y_true, y_predicated)
-
-      # OR to log a final metric at the end of training you can also use wandb.summary
-
-      # https://docs.wandb.ai/guides/integrations/scikit
-      wandb.run.summary["accuracy"] = accuracy
-      wandb.run.summary["f1"] = f1
-      wandb.run.summary["recall"] = recall
-      wandb.run.summary["precision"] = precision
-      wandb.run.summary["auc"] = auc
-      wandb.sklearn.plot_roc(y_true, y_predicated, [0, 1])
-      wandb.log(
-          {
-              "conf_matrix": wandb.plot.confusion_matrix(
-                  probs=None, y_true=y_true, preds=y_predicated, class_names=[0, 1],
-              )
-          }
-      )
-  elif mode=='test':
-    wandb.watch(model, log_freq=100)
 
       test_size = len(test_loader.dataset)
       batch_size = test_loader.batch_size
@@ -142,7 +143,7 @@ def main():
     )
     parser.add_argument(
         "--model",
-        choices=architecture.models.keys(),
+        choices=architecture.models_dict.keys(),
         required=True,
         help="Architecture to test. Check architecture.py",
     )
@@ -165,7 +166,7 @@ def main():
     wandb.config.update(args)
 
     device = get_device()
-    input_transforms, model_klass = architecture.models[args.model]
+    input_transforms, model_klass = architecture.models_dict[args.model]
     model = model_klass()
     model.load_state_dict(torch.load(args.model_state_file))
     if args.mode =='val':
